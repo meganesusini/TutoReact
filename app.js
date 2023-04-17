46a7489228ef0dd2f1762c634c769e67e9f03261
megane <susini.megane04@gmail.com>
2023-04-17 17:12:39 +0200

    first part chap6 finished

--- app.js
+++ app.js
@@ -1,142 +1,31 @@
-function WelcomeFunc({name, children}) {
-    return <div>
-        <h1>Bonjour {name}</h1>
-        <p>
-            {children}
-        </p>
-    </div>
-}
-
-class Welcome extends React.Component {
-
-    render() {
-        return <div>
-        <h1>Bonjour {this.props.name}</h1>
-        <p>
-            {this.props.children}
-        </p>
-    </div>
-    }
-}
-
-class Clock extends React.Component {
+class Home extends React.Component {
 
     constructor (props) {
         super(props)
-        this.state = {date: new Date()}
-        this.timer = null
+        this.state = {
+            nom : 'demo2'
+        }
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    componentDidMount () {
-        this.timer = window.setInterval(this.tick.bind(this), 1000) 
-    }
-
-    componentWillUnmount () {
-        window.clearInterval(this.timer)
-    }
-
-    tick () {
-        this.setState({date: new Date()})
+    handleChange (e) {
+        this.setState({
+            nom : e.target.va
+        })
     }
 
     render () {
         return <div>
-            Il est {this.state.date.toLocaleDateString()} {this.state.date.toLocaleTimeString()}
+            <label htmlFor = "nom">Mon nom</label>
+            {/* <input type="text" id="nom" name="nom" value={this.state.nom} onChange={this.handleChange}/> */}
+            <select value={this.state.nom} onChange={this.handleChange}>
+                <option value="demo1">Demo 1</option>
+                <option value="demo2">Demo 2</option>
+                <option value="demo3">Demo 3</option>
+            </select>
         </div>
     }
 }
-
-class Incrementer extends React.Component {
-
-    constructor (props) {
-        super(props)
-        this.state = {n: props.start, timer: null}
-        this.toggle = this.toggle.bind(this)
-        this.reset = this.reset.bind(this)
-    }
-
-    componentDidMount () {
-        this.play()
-    }
-
-    componentWillUnmount (e) {
-        window.clearInterval(this.state.timer)
-    }
-
-    increment () {
-        this.setState(function (state, props) {
-            return {n : state.n + props.step}            
-        })
-    }
-
-    pause () {
-        window.clearInterval(this.state.timer)
-        this.setState({
-            timer : null
-        })
-    }
-
-    play () {
-        window.clearInterval(this.state.timer)
-        this.setState({
-            timer : window.setInterval(this.increment.bind(this), 1000)
-        })
-    }
-
-    toggle () {
-        return this.state.timer ? this.pause() : this.play()
-    }
-
-    label () {
-        return this.state.timer ? 'Pause' : 'Lecture'
-    }
-
-    reset () {
-        this.pause()
-        this.play()
-        this.setState((state, props) =>({n : props.start}))
-    }
-
-    render () {
-        return <div>
-            Valeur : {this.state.n}
-            <button onClick={this.toggle}>{this.label()}</button>
-            <button onClick={this.reset}>Réinitialiser</button>
-        </div>
-    }
-
-}
-
-Incrementer.defaultProps = {
-    start : 0,
-    step : 1
-}
-
-class ManualIncrementer extends React.Component {
-
-    constructor (props) {
-        super(props)
-        this.state = {n: 0}
-    }
-
-    increment (e) {
-        e.preventDefault
-        this.setState(function (state, props) {
-            return {n : state.n + 1}            
-        })
-    }
-
-    render () {
-        return <div>Valeur : {this.state.n} <button onClick={this.increment.bind(this)}>Incrémenter</button></div>
-    }
-}
-
-function Home () {
-    return <div>
-        <Welcome name="Dorothée" />
-        <Welcome name="Jean" />
-        <Incrementer/>
-    </div>
-}
-
 ReactDOM.render(<Home />, document.querySelector('#app'))
+
+// CHAP6 6:50
